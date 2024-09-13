d32d3ba637d250f922432361f5667f38cfc37400
kevinjamiesondev <kevinjamiesondev@gmail.com>
2024-09-13 09:02:30 -0500

    Added variables for the buttons and time values

---
+++ script.js
@@ -0,0 +1,11 @@
+// Variables for buttons
+
+const startStopBtn = document.querySelector('#startStopBtn');
+const resetBtn = document.querySelector('#resetBtn');
+
+// Variables for time values
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
